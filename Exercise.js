db22b0fbad5b2ec502059af6ae3e8c681647cf78
asm-saim <saim777m@gmail.com>
2026-05-24 22:08:20 +0600

    shortcut and exercise added

---
+++ Exercise.js
@@ -0,0 +1,33 @@
+// Ex - 1
+let burgerPrice=600;
+if(burgerPrice>500)
+{
+    console.log("Free Coke");
+}
+else{
+    console.log("Coke is 30TK");
+}
+
+// Ex -2 
+// you have two numbers in two variables, called: num1, num2
+// now declare a variable called result. 
+// if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+// write a simple if-else. 
+
+// also, write it using ternary operator.
+
+let num1=10;
+let num2= 20;
+num1>num2? result = num1*2 :  result = num1 + num2;
+console.log(result);
+// let result
+// if(num1>num2)
+// {
+//     result=num1*2;
+//     console.log(result);
+// }
+// else{
+//     result= num1 + num2;
+//     console.log(result);
+// }
---
+++ shortcut.js
@@ -0,0 +1,27 @@
+const price= 300;
+const isDiscount=true;
+// if(isDiscount===true)
+if(isDiscount)
+{
+    console.log("This is discounted product");
+}
+else{
+    console.log("No Discount");
+}
+
+// Ex: 2
+
+const food=300;
+const isSafe=false;
+// if(isSafe===false)
+if(!isSafe)
+{
+    console.log("Do not buy");
+}
+else{
+    console.log("You can buy");
+}
+
+// logical Not
+// true -> !true = false
+// true -> !!true = true
